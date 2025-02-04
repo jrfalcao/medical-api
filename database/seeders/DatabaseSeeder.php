cad5190be758e3516867750a1c2ec18f5ca87254
Junior Falcão <juniorfalcao.jc@gmail.com>
2025-02-04 10:17:46 -0300

    Atualiza DatabaseSeeder para usar factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        \App\Models\Cidade::factory(10)->create();
+        \App\Models\Medico::factory(10)->create();
+        \App\Models\Paciente::factory(10)->create();
+        \App\Models\Consulta::factory(30)->create();
     }
 }
